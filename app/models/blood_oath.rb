ac2bc1b2a258d4b7f2b8f44ad6c9064ab9802a7b
Fraciscv <Francisco.Andaur@icloud.com>
2020-06-28 00:44:38 -0400

    ugh

--- app/models/blood_oath.rb
+++ app/models/blood_oath.rb
@@ -1,16 +1,17 @@
 class BloodOath
-    attr_reader = :initiation_date, :cult, :follower
+    attr_reader :initiation_date, :cult, :follower
     @@all = []
-
-    def initialize(date, cult, follower)
-        @initiation_date = date
+    def initialize(cult, follower)
+        @initiation_date = Time.now.to_s.split[0]
         @cult = cult
-        @follower = follower
-        @all << self
+        @follower = follower 
+        @@all << self 
     end
-
+    #returns an Array of all the blood oaths
     def self.all
-        @all
+        @@all 
     end
-
+    def self.first_oath
+        self.all[0].follower
+    end
 end
